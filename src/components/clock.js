043007b9910e7fb33e1043bc71f9174af24413a8
Piper Pillar <piper.pillar@gmail.com>
2018-07-16 16:34:07 +0000

    built and styled clock component

---
+++ src/components/clock.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+
+class Clock extends Component {
+    render() {
+        return (
+            <div className="clock">
+                <div className="clock__days">
+                    <label className="clock__title">DAYS</label>
+                    <label className="clock__amount">20</label>
+                </div>
+
+                <div className="clock__hours">
+                    <label className="clock__title">HOURS</label>
+                    <label className="clock__amount">20</label>
+                </div>
+
+                <div className="clock__minutes">
+                    <label className="clock__title">MIN</label>
+                    <label className="clock__amount">20</label>
+                </div>
+            
+                <div className="clock__seconds">
+                    <label className="clock__title">SEC</label>
+                    <label className="clock__amount">20</label>
+                </div>
+            </div>
+            );
+    }
+}
+
+export default Clock;
