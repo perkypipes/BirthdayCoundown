8a8f3c7900bc0a96d2a416ea36e0cbcf7daee57a
Piper Pillar <piper.pillar@gmail.com>
2018-07-16 15:16:48 +0000

    installed moment.js, react date picker, and placed picker component in grid

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Picker from './picker';
 
 export default class App extends Component {
   render() {
     return (
       <div className="grid">
       <h1 className="grid__title">Birthday Countdown</h1>
+      
        <div className="grid__skew-dark-one-box"></div>
        <div className="grid__skew-dark-two"></div>
        <div className="grid__skew-dark-three"></div>
@@ -12,6 +14,8 @@ export default class App extends Component {
        <div className="grid__skew-light-one"></div>
        <div className="grid__skew-light-two"></div>
        <div className="grid__skew-light-three-box"></div>
+       
+       <Picker/> 
       </div>
     );
   }
