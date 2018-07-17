512f598cafc22ce3d2c4b6a617aba1fc0896284c
Piper Pillar <piper.pillar@gmail.com>
2018-07-17 16:14:48 +0000

    fixed wink wink timer state bug and got timer object into clockjs jsx

--- src/components/app.js
+++ src/components/app.js
@@ -1,4 +1,124 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+ 
+import Picker from "./picker";
+import Button from "./button";
+import Clock from "./clock";
+import ChangeDate from "./changeDate";
+import LargeText from "./largeText";
+ 
+import moment from "moment";
+ 
+ export default class App extends Component {
+     
+   constructor(props) {
+       
+    super(props);
+ 
+    this.timer = 0;
+ 
+     this.state = {
+       active: false,
+       startDate: moment(),
+       timeRemaining: {
+         days: 0,
+        hours: 0,
+         minutes: 0,
+         seconds: 0
+       }
+    };
+ 
+    this.handleGenerate = this.handleGenerate.bind(this);
+   }
+ 
+   handleChange = function(date) {
+    console.log("APP JS HANDLE CHANGE", date._d);
+     this.setState({
+       startDate: date
+     });
+  }.bind(this);
+ 
+   handleGenerate = function() {
+    this.setState({ active: true });
+    // Set the date we're counting down to
+
+     var countDownDate = this.state.startDate.toDate().getTime();
+ 
+     // Update the count down every 1 second
+    this.timer = setInterval(function() {
+      // Get todays date and time
+      var now = new Date().getTime();
+
+      // Find the distance between now an the count down date
+      var distance = countDownDate - now;
+
+      // Time calculations for days, hours, minutes and seconds
+      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      var hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      // Output the result in an element with id="demo"
+      const time = days  "d "  hours  "h "  minutes  "m "  seconds  "s ";
+       const timeRemaining = {
+         days,
+         hours,
+         minutes,
+         seconds
+      };
+      this.setState({ timeRemaining });
+
+      // If the count down is over, write some text
+      if (distance < 0) {
+        clearInterval(this.timer);
+         // document.getElementById("demo").innerHTML = "EXPIRED";
+      }
+    }.bind(this), 1000);
+  }.bind(this);
+ 
+   renderItems = function() {
+    if (this.state.active) {
+       return [
+        <Clock timeRemaining={this.state.timeRemaining} />,
+        ChangeDate("Change Date", () => this.setState({ active: false })),
+        LargeText("04/03"),
+        <label className="grid__remaining">
+          Remaining until your 21st birthday
+        </label>
+      ];
+     } else {
+       return [
+        <Picker
+          startDate={this.state.startDate}
+          callback={date => this.handleChange(date)}
+        />,
+        Button("Generate Countdown", () => this.handleGenerate())
+      ];
+     }
+  }.bind(this);
+ 
+   render() {
+     return (
+       <div className="grid">
+         <h1 className="grid__title">Birthday Countdown</h1>
+        <div className="grid__skew-dark-two" />
+        <div className="grid__skew-dark-three" />
+ 
+
+        <div className="grid__skew-light-one" />
+        <div className="grid__skew-light-two" />
+        <div className="grid__skew-light-three-box" />
+ 
+        {this.renderItems()}
+       </div>
+     );
+   }
+}
+
+
+
+/*import React, { Component } from 'react';
 import Picker from './picker';
 import Button from './button';
 import Clock from './clock';
@@ -11,7 +131,8 @@ export default class App extends Component {
     
     constructor(props) {
         super(props)
-        
+       
+       this.timer = 0; 
 
         this.state = {
             active: false,
@@ -39,7 +160,7 @@ export default class App extends Component {
         var countDownDate = this.stat.startDate.toDate().getTime();
         
         //update the count down every 1 second
-        var x = setInterval(function () {
+        this.timer = setInterval(function () {
             
         // Get todays date and time
         var now = new Date().getTime();
@@ -54,21 +175,23 @@ export default class App extends Component {
          var seconds = Math.floor((distance % (1000 * 60)) / 1000);
      
          // Output the result in an element with id="demo"
-         const time = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
-         
-         this.setState ({
-              timeRemaining: {
+         const time = days  "d "  hours  "h "  minutes  "m "  seconds  "s ";
+         const timeRemaining = {
                 days,
                 hours,
                 minutes,
                 seconds
             }
+            
+         this.setState ({
+             //timeRemaining
+             active: this.state.active
          })
          console.log(this.state.timeRemaining)
  
         // If the count down is over, write some text 
         if (distance < 0) {
-         clearInterval(x);
+         clearInterval(timer);
          // document.getElementById("demo").innerHTML = "EXPIRED";
             
         }
@@ -108,3 +231,4 @@ export default class App extends Component {
     );
   }
 }
+*/
--- src/components/picker.js
+++ src/components/picker.js
@@ -1,4 +1,26 @@
-/*date picker*/
+import React, { Component } from "react";
+ 
+ import DatePicker from "react-datepicker";
+ import moment from "moment";
+ 
+ import 'react-datepicker/dist/react-datepicker.css';
+ 
+ class Picker extends Component {
+ 
+  handleChange = function(date) {
+     this.props.callback(date)
+  }.bind(this);
+ 
+   render() {
+     return (
+       <div className="picker">
+         <DatePicker
+          selected={this.props.startDate}
+           onChange={this.handleChange}
+         />
+       </div>
+
+/*date picker
 import React, { Component } from 'react';
 
 import DatePicker from 'react-datepicker';
@@ -24,4 +46,4 @@ class Picker extends Component {
             )
     }
 }
-export default Picker;
+export default Picker;*/
