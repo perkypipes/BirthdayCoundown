20bfd773096b874815e2b53edc4bd22bbeea6ac4
Piper Pillar <piper.pillar@gmail.com>
2018-07-18 15:52:46 +0000

    fixed the little bugs! finished now

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,3 @@
-/* 8:41 minutes into the video */
-
 import React, { Component } from "react";
  
 import Picker from "./picker";
@@ -11,37 +9,37 @@ import LargeText from "./largeText";
 import moment from "moment";
  
  export default class App extends Component {
-     
-   constructor(props) {
-       
+  constructor(props) {
     super(props);
- 
+
     this.timer = 0;
- 
-     this.state = {
-       active: false,
-       startDate: moment(),
-       timeRemaining: {
-         days: 0,
+
+    this.state = {
+      active: false,
+      startDate: moment(),
+      timeRemaining: {
+        days: 0,
         hours: 0,
-         minutes: 0,
-         seconds: 0
-       },
-       age: 0
+        minutes: 0,
+        seconds: 0
+      },
+      age: 0
     };
+
  
     this.handleGenerate = this.handleGenerate.bind(this);
    }
  
    handleChange = function(date) {
     console.log("APP JS HANDLE CHANGE", date._d);
-     clearInterval(this.timer);
+    
      this.setState({
        startDate: date
      });
   }.bind(this);
  
    handleGenerate = function() {
+     clearInterval(this.timer)
     
     var bday = this.state.startDate.toDate();
     var today = new Date();
@@ -76,7 +74,7 @@ import moment from "moment";
  
      this.timer = setInterval(function() {
  
-       var now = today.getTime();
+       var now = moment().toDate().getTime();
        var distance = countDownDate - now;
  
        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
@@ -93,9 +91,9 @@ import moment from "moment";
          minutes,
          seconds
        };
+       
        this.setState({ timeRemaining });
 
-      // If the count down is over, write some text
       if (distance < 0) {
         clearInterval(this.timer);
          // document.getElementById("demo").innerHTML = "EXPIRED";
@@ -104,12 +102,12 @@ import moment from "moment";
   }.bind(this);
   
   getBirthDate = function(date) {
-      const month = date.getMonth + 1;
-      const day = date.getDate();
-      if(month < 10) {
-          return `0${month}/ ${day}`
-      }
-      return `${month}/ ${day}`
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    if(month < 10) {
+      return `0${month}/${day}`
+    }
+      return `${month}/${day}`
   }.bind(this);
  
    renderItems = function() {
@@ -117,7 +115,7 @@ import moment from "moment";
        return [
         <Clock key={0} timeRemaining={this.state.timeRemaining} />,
         ChangeDate("Change Date", () => this.setState({ active: false })),
-        LargeText(this.getBirthDate(this.startDate.toDate())),
+        LargeText(this.getBirthDate(this.state.startDate.toDate())),
         <label key={3} className="grid__remaining">
           Remaining until you turn {this.state.age}
         </label>
@@ -151,120 +149,3 @@ import moment from "moment";
      );
    }
 }
-
-
-
-/*import React, { Component } from 'react';
-import Picker from './picker';
-import Button from './button';
-import Clock from './clock';
-import ChangeDate from './changeDate';
-import LargeText from './largeText';
-
-import Moment from 'moment';
-
-export default class App extends Component {
-    
-    constructor(props) {
-        super(props)
-       
-       this.timer = 0; 
-
-        this.state = {
-            active: false,
-            startDate: moment(),
-            timeRemaining: {
-                days: 0,
-                hours: 0,
-                minutes: 0,
-                seconds: 0
-            }
-        }
-    }
-    
-    handleGenerate = function(date) {
-       console.log('APP JS HANDLE CHANGE', date._d);
-       this.setState({
-        startDate: date
-     });
-    }.bind(this)
-    
-    handleGenerate = function() {
-        this.setState({active: true})
-        //set the date we're counting down to
-        
-        var countDownDate = this.stat.startDate.toDate().getTime();
-        
-        //update the count down every 1 second
-        this.timer = setInterval(function () {
-            
-        // Get todays date and time
-        var now = new Date().getTime();
-     
-        // Find the distance between now an the count down date
-        var distance = countDownDate - now;
-     
-         // Time calculations for days, hours, minutes and seconds
-         var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-         var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-         var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-         var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-     
-         // Output the result in an element with id="demo"
-         const time = days  "d "  hours  "h "  minutes  "m "  seconds  "s ";
-         const timeRemaining = {
-                days,
-                hours,
-                minutes,
-                seconds
-            }
-            
-         this.setState ({
-             //timeRemaining
-             active: this.state.active
-         })
-         console.log(this.state.timeRemaining)
- 
-        // If the count down is over, write some text 
-        if (distance < 0) {
-         clearInterval(timer);
-         // document.getElementById("demo").innerHTML = "EXPIRED";
-            
-        }
-    }, 1000);
-    }.bind(this)
-    
-    renderItems = function() {
-        if(this.state.active) {
-            return [
-                <Clock/>,
-                ChangeDate('Change Date',() => this.setState({active: false})),
-                LargeText('04/03'),
-                <label className="grid__remaining">Remaining until your 20th Birthday</label>
-            ]
-        } else {
-            return [
-                <Picker startDate={this.state.startDate} callback={(date) => this.handleChange(date)}/>,
-                Button('Generate Countdown', () => this.setState({active: true}))
-                ] 
-        }
-    }.bind(this)
-    
-  render() {
-    return (
-      <div className="grid">
-      <h1 className="grid__title">Birthday Countdown</h1>
-      
-       <div className="grid__skew-dark-two"></div>
-       <div className="grid__skew-dark-three"></div>
-       
-       <div className="grid__skew-light-one"></div>
-       <div className="grid__skew-light-two"></div>
-       <div className="grid__skew-light-three-box"></div>
-       
-       { this.renderItems() }
-      </div>
-    );
-  }
-}
-*/
